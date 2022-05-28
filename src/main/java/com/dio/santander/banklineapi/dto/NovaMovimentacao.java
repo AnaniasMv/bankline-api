1abe663d0f3687bdd378a85a0d3854e137276b9e
AnaniasMv <ananiasmatheus22@outlook.com>
2022-05-28 13:35:40 -0300

    versão final do projeto

--- src/main/java/com/dio/santander/banklineapi/dto/NovaMovimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/dto/NovaMovimentacao.java
@@ -1,5 +1,40 @@
 package com.dio.santander.banklineapi.dto;
 
-public class NovaMovimentacao {
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
+import com.dio.santander.banklineapi.model.Movimentacaotipo;
+
+public class NovaMovimentacao {
+private String descricao;
+	private Double valor;
+	private Movimentacaotipo tipo;
+	private Integer idConta;
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public Movimentacaotipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(Movimentacaotipo tipo) {
+		this.tipo = tipo;
+	}
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
+	
+	
 }
--- src/main/java/com/dio/santander/banklineapi/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/banklineapi/service/MovimentacaoService.java
@@ -1,5 +1,43 @@
 package com.dio.santander.banklineapi.service;
 
-public class MovimentacaoService {
+import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.model.Correntista;
+import com.dio.santander.banklineapi.model.Movimentacao;
+import com.dio.santander.banklineapi.model.Movimentacaotipo;
+import com.dio.santander.banklineapi.repository.CorrentistaRepository;
+import com.dio.santander.banklineapi.repository.MovimentacaoRepository;
+
+@Service
+public class MovimentacaoService {
+	@Autowired
+	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
+	public void save(NovaMovimentacao novaMovimentacao) {
+		Movimentacao movimentacao = new Movimentacao();
+		
+		//Double valor = novaMovimentacao.getTipo()==Movimentacaotipo.RECEITA ? novaMovimentacao.getValor() : novaMovimentacao.getValor() * -1;
+		
+		Double valor = novaMovimentacao.getValor();
+		if(novaMovimentacao.getTipo() == Movimentacaotipo.DESPESA)
+			 valor = valor *-1;
+		
+		movimentacao.setDataHora(LocalDateTime.now());
+		movimentacao.setDescricao(novaMovimentacao.getDescricao());
+		movimentacao.setIdConta(novaMovimentacao.getIdConta());
+		movimentacao.setTipo(novaMovimentacao.getTipo());
+		movimentacao.setValor(novaMovimentacao.getValor());
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if(correntista != null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			correntistaRepository.save(correntista);
+		}
+		repository.save(movimentacao);
+	}
 }
