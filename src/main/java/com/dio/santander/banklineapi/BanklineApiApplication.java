5318a14acbf5fb5cce954263beceff723403d6b7
AnaniasMv <ananiasmatheus22@outlook.com>
2022-05-31 16:24:57 -0300

    Versão final do projeto

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -2,13 +2,13 @@ package com.dio.santander.banklineapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		//https://dio-bankline-api.herokuapp.com/swagger-ui/index.html
 	}
+
 }
